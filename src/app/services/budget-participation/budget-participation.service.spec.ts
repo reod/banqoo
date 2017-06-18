1799a030b3ba44325407bdab06e2cd896ed4e3f7
Remigiusz Ambroziak <remigiuszambroziak@remek-mac.local>
2017-06-18 11:14:45 +0200

    Simple test for BPS

--- src/app/services/budget-participation/budget-participation.service.spec.ts
+++ src/app/services/budget-participation/budget-participation.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { BudgetParticipationService } from './budget-participation.service';
+import { BudgetParticipationService, Participant } from './budget-participation.service';
 
 describe('BudgetParticipationService', () => {
   beforeEach(() => {
@@ -11,5 +11,15 @@ describe('BudgetParticipationService', () => {
 
   it('should be created', inject([BudgetParticipationService], (service: BudgetParticipationService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should return equal participations when incomes are equal', inject([BudgetParticipationService], (service: BudgetParticipationService) => {
+    const participantA = <Participant> { name: 'A', income: 200 };
+    const participantB = <Participant> { name: 'B', income: 200 };
+    const participants = <Participant[]> [participantA, participantB];
+
+    const [participationA, participationB] = service.getParticipation(400, participants);
+    
+    expect(participationA.participation).toEqual(participationB.participation);
   }));
 });
